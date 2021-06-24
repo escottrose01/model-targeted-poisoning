21ed8f1e398fd0dc37c0f6c40a5c0514dfba52b0
Evan Rose <escottrose01@gmail.com>
2021-06-24 12:02:17 -0400

    Add feature subpop code to generate_target_theta_equation2.py

--- generate_target_theta_equation2.py
+++ generate_target_theta_equation2.py
@@ -7,13 +7,18 @@ import numpy as np
 
 from sklearn import linear_model, svm
 # from utils import *
-from utils import svm_model, calculate_loss, dist_to_boundary, cvx_dot
+from utils import svm_model, calculate_loss, dist_to_boundary, cvx_dot, get_subpop_inds
 import cvxpy as cvx
 
 import pickle
 import argparse
 from datasets import load_dataset
 import os
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--subpop_type', default='cluster', choices=['cluster', 'feature'], help='subpopulaton type: cluster or feature')
+args = parser.parse_args()
+subpop_type = args.subpop_type
 
 # for now, will only be trying to do subpopulation on adult
 dataset_name = 'adult'
@@ -79,22 +84,22 @@ class search_target_theta(object):
         # define the bias and variable terms
         X_train, y_train = D_c
         x_sub, y_sub = D_sub
-        
+
         d = X_train.shape[1] # dimension of theta
-        
+
         self.cvx_theta_p = cvx.Variable(d)
-        self.cvx_bias_p = cvx.Variable() 
+        self.cvx_bias_p = cvx.Variable()
 
         #ALPHA VALUE
         self.alpha = cvx.Parameter(sign='positive')
 #        self.alpha = .1
-        
+
         reg = cvx.pnorm(self.cvx_theta_p, 2)**2
         self.L = cvx.sum_entries(cvx.pos(1 - cvx.mul_elemwise(y_train, X_train * self.cvx_theta_p + self.cvx_bias_p)))/X_train.shape[0] + (weight_decay/2) * reg
         self.M = cvx.sum_entries(cvx.pos(1 - cvx.mul_elemwise(y_sub, x_sub * self.cvx_theta_p + self.cvx_bias_p)))/x_sub.shape[0] + + (weight_decay/2) * reg
 
         self.equation_two = self.L + (self.alpha * self.M)
-        
+
         self.cvx_objective = cvx.Minimize(self.equation_two)
         self.cvx_prob = cvx.Problem(self.cvx_objective)
 
@@ -104,10 +109,10 @@ class search_target_theta(object):
             ):
 
         self.alpha.value = alpha
-        
-        self.cvx_prob.solve(verbose=verbose, solver=cvx.GUROBI) 
-        
-        target_theta = np.array(self.cvx_theta_p.value) 
+
+        self.cvx_prob.solve(verbose=verbose, solver=cvx.GUROBI)
+
+        target_theta = np.array(self.cvx_theta_p.value)
         target_theta = target_theta.reshape(-1)
         target_bias = np.array(self.cvx_bias_p.value)
 
@@ -122,82 +127,73 @@ from sklearn import cluster
 num_clusters = 20
 pois_rates = [0.03,0.05,0.1,0.15,0.2,0.3,0.4,0.5]
 
-cls_fname = 'files/data/{}_trn_cluster_labels.txt'.format(dataset_name)
-if os.path.isfile(cls_fname):
-    trn_km = np.loadtxt(cls_fname)
-    cls_fname = 'files/data/{}_tst_cluster_labels.txt'.format(dataset_name)
-    tst_km = np.loadtxt(cls_fname)
-else:
-    num_clusters = 20
-    km = cluster.KMeans(n_clusters=num_clusters,random_state = 0)
-    km.fit(X_train)
-    trn_km = km.labels_
-    tst_km = km.predict(X_test)
-    # save the clustering info to ensure everything is reproducible
-    cls_fname = 'files/data/{}_trn_cluster_labels.txt'.format(dataset_name)
-    np.savetxt(cls_fname,trn_km)
-    cls_fname = 'files/data/{}_tst_cluster_labels.txt'.format(dataset_name)
-    np.savetxt(cls_fname,tst_km)
 # find the clusters and corresponding subpop size
-cl_inds, cl_cts = np.unique(trn_km, return_counts=True)
+trn_subpop_fname = 'files/data/{}_trn_{}_labels.txt'.format(dataset_name, subpop_type)
+with open(trn_subpop_fname, 'r') as f:
+    trn_all_subpops = [np.array(map(int, line.split())) for line in f]
+tst_subpop_fname = 'files/data/{}_tst_{}_labels.txt'.format(dataset_name, subpop_type)
+with open(tst_subpop_fname, 'r') as f:
+    tst_all_subpops = [np.array(map(int, line.split())) for line in f]
+
+subpops_flattened = np.concatenate(trn_all_subpops).flatten()
+subpop_inds, subpop_cts = np.unique(subpops_flattened, return_counts=True)
+
 trn_sub_accs = []
-for i in range(len(cl_cts)):
-    cl_ind, cl_ct = cl_inds[i], cl_cts[i]
-    print("cluster ID and Size:",cl_ind,cl_ct)         
-    # indices of points belong to cluster
-    tst_sbcl = np.where(np.logical_and(tst_km==cl_ind,Y_test == -1))
-    trn_sbcl = np.where(np.logical_and(trn_km==cl_ind,Y_train == -1))
-    tst_non_sbcl = np.where(np.logical_or(tst_km!=cl_ind,Y_test != -1))
-    trn_non_sbcl = np.where(np.logical_or(trn_km!=cl_ind,Y_train != -1))
-    
+for i in range(len(subpop_cts)):
+    subpop_ind, subpop_ct = subpop_inds[i], subpop_cts[i]
+    print("subpop ID and Size:",subpop_ind,subpop_ct)
+    # indices of points belong to subpop
+    tst_subpop_inds = np.array([np.any(v == subpop_ind) for v in tst_all_subpops])
+    trn_subpop_inds = np.array([np.any(v == subpop_ind) for v in trn_all_subpops])
+    tst_sbcl, trn_sbcl, tst_non_sbcl, trn_non_sbcl = get_subpop_inds(dataset_name, tst_subpop_inds, trn_subpop_inds, Y_test, Y_train)
+
     # get the corresponding points in the dataset
     tst_sub_x, tst_sub_y = X_test[tst_sbcl], Y_test[tst_sbcl]
     tst_nsub_x, tst_nsub_y = X_test[tst_non_sbcl], Y_test[tst_non_sbcl]
     trn_sub_x, trn_sub_y  = X_train[trn_sbcl], Y_train[trn_sbcl]
-    trn_nsub_x, trn_nsub_y = X_train[trn_non_sbcl], Y_train[trn_non_sbcl]  
+    trn_nsub_x, trn_nsub_y = X_train[trn_non_sbcl], Y_train[trn_non_sbcl]
     tst_sub_acc = model.score(tst_sub_x, tst_sub_y)
     trn_sub_acc = model.score(trn_sub_x, trn_sub_y)
     # check the target and collateral damage info
-    print("----------Subpop Indx: {} ------".format(cl_ind))
+    print("----------Subpop Indx: {} ------".format(subpop_ind))
     print('Clean Train Target Acc : %.3f' % model.score(trn_sub_x, trn_sub_y))
     print('Clean Train Collat Acc : %.3f' % model.score(trn_nsub_x,trn_nsub_y))
     print('Clean Test Target Acc : %.3f' % tst_sub_acc)
     print('Clean Test Collat Acc : %.3f' % model.score(tst_nsub_x, tst_nsub_y))
     trn_sub_accs.append(trn_sub_acc)
 
-print(cl_inds, cl_cts)
+print(subpop_inds, subpop_cts)
 # print(tst_sub_accs)
 # sort the subpop based on tst acc and choose 5 highest ones
 highest_5_inds = np.argsort(trn_sub_accs)[-3:]
 #highest_5_inds = np.argsort(tst_sub_accs)[-5:]
-cl_inds = cl_inds[highest_5_inds]
-cl_cts = cl_cts[highest_5_inds]
-print(cl_inds, cl_cts)
+subpop_inds = subpop_inds[highest_5_inds]
+subpop_cts = subpop_cts[highest_5_inds]
+print(subpop_inds, subpop_cts)
 
 # save the selected subpop info
-cls_fname = 'files/data/{}_svm_selected_subpops.txt'.format(dataset_name)
-np.savetxt(cls_fname,np.array([cl_inds,cl_cts]))
+cls_fname = 'files/data/{}_svm_{}_selected_subpops.txt'.format(dataset_name, subpop_type)
+np.savetxt(cls_fname,np.array([subpop_inds,subpop_cts]))
 print("#---------Selected Subpops------#")
-for i in range(len(cl_cts)): 
-    cl_ind, cl_ct = cl_inds[i], cl_cts[i]
-    print("cluster ID and Size:",cl_ind,cl_ct)
-    # indices of points belong to cluster
-    tst_sbcl = np.where(np.logical_and(tst_km==cl_ind,Y_test == -1))[0]
-    trn_sbcl = np.where(np.logical_and(trn_km==cl_ind,Y_train == -1))[0]
-    tst_non_sbcl = np.where(np.logical_or(tst_km!=cl_ind,Y_test != -1))[0]
-    trn_non_sbcl = np.where(np.logical_or(trn_km!=cl_ind,Y_train != -1))[0]
-    
+for i in range(len(subpop_cts)):
+    subpop_ind, subpop_ct = subpop_inds[i], subpop_cts[i]
+    print("cluster ID and Size:",subpop_ind,subpop_ct)
+    # indices of points belong to subpop
+    tst_subpop_inds = np.array([np.any(v == subpop_ind) for v in tst_all_subpops])
+    trn_subpop_inds = np.array([np.any(v == subpop_ind) for v in trn_all_subpops])
+    tst_sbcl, trn_sbcl, tst_non_sbcl, trn_non_sbcl = get_subpop_inds(dataset_name, tst_subpop_inds, trn_subpop_inds, Y_test, Y_train)
+
     # get the corresponding points in the dataset
     tst_sub_x, tst_sub_y = X_test[tst_sbcl], Y_test[tst_sbcl]
     tst_nsub_x, tst_nsub_y = X_test[tst_non_sbcl], Y_test[tst_non_sbcl]
     trn_sub_x, trn_sub_y  = X_train[trn_sbcl], Y_train[trn_sbcl]
-    trn_nsub_x, trn_nsub_y = X_train[trn_non_sbcl], Y_train[trn_non_sbcl]  
+    trn_nsub_x, trn_nsub_y = X_train[trn_non_sbcl], Y_train[trn_non_sbcl]
     tst_sub_acc = model.score(tst_sub_x, tst_sub_y)
     # make sure subpop is from class -1
     assert (tst_sub_y == -1).all()
     assert (trn_sub_y == -1).all()
     # check the target and collateral damage info
-    print("----------Subpop Indx: {}------".format(cl_ind))
+    print("----------Subpop Indx: {}------".format(subpop_ind))
     print('Clean Total Train Acc : %.3f' % model.score(X_train, Y_train))
     print('Clean Train Target Acc : %.3f' % model.score(trn_sub_x, trn_sub_y))
     print('Clean Train Collat Acc : %.3f' % model.score(trn_nsub_x,trn_nsub_y))
@@ -216,7 +212,7 @@ for i in range(len(cl_cts)):
     model_dumb.intercept_ = np.array([target_bias])
     # print out the acc info on subpop
     trn_sub_acc = model_dumb.score(trn_sub_x, trn_sub_y)
-    # print("----------Subpop Indx: {}------".format(cl_ind))
+    # print("----------Subpop Indx: {}------".format(subpop_ind))
     print('Target Total Train Acc : %.3f' % model_dumb.score(X_train, Y_train))
     print('Target Train Target Acc : %.3f' % model_dumb.score(trn_sub_x, trn_sub_y))
     print('Target Train Collat Acc : %.3f' % model_dumb.score(trn_nsub_x,trn_nsub_y))
@@ -228,10 +224,10 @@ for i in range(len(cl_cts)):
 
     data_all = {}
     data_all['thetas'] = target_theta
-    data_all['biases'] = target_bias      
+    data_all['biases'] = target_bias
     # save the target thetas
-    if not os.path.isdir('files/target_classifiers/{}/svm'.format(dataset_name)):
-        os.makedirs('files/target_classifiers/{}/svm'.format(dataset_name))
-    file_all = open('files/target_classifiers/{}/svm/orig_best_theta_subpop_{}_err-1.0'.format(dataset_name,int(cl_ind)), 'wb')
+    if not os.path.isdir('files/target_classifiers/{}/svm/{}'.format(dataset_name, subpop_type)):
+        os.makedirs('files/target_classifiers/{}/svm/{}'.format(dataset_name, subpop_type))
+    file_all = open('files/target_classifiers/{}/svm/{}/orig_best_theta_subpop_{}_err-1.0'.format(dataset_name, subpop_type, int(subpop_ind)), 'wb')
     pickle.dump(data_all, file_all,protocol=2)
     file_all.close()
